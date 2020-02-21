22c7c3e5bbb58ccb45c960b4fa20bd76311f3f2a
bit <bit@DESKTOP-ULIDFS7>
2020-02-21 13:31:21 +0900

    emaillist03 add

--- emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -1,19 +1,44 @@
 package com.douzone.emaillist.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.emaillist.repository.EmaillistRepository;
+import com.douzone.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
 
 	
-	@ResponseBody
+	@Autowired
+	private EmaillistRepository emaillistRepository;
+	
+
+	
 	@RequestMapping("")
-	public String index() {
-		
-		return "hello";
-		
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list",list);
+		return "index";
+	}
+	
+	@RequestMapping(value="/add",method = RequestMethod.GET)
+	public String add() {
+		return "add";
+	}
+	
+	@RequestMapping(value="/add",method = RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
 	}
 	
 }
